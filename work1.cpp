6e5f8f5f218a61fe2dc9d516314403f3fc73e116
billcsiegood <billongood@gmail.com>
2013-12-18 01:54:28 +0800

    work1-2

--- work1.cpp
+++ work1.cpp
@@ -1,16 +1,7 @@
 #include<stdio.h>         
 #include<stdlib.h>         
 main()   
-{ int a,b,temp;   
- while(scanf("%d %d",&a,&b)==2)   
- {
- while(a%b)   
-  {   
-   temp=a;   
-   a=b;   
-   b=temp%b;              
-  }   
-  printf("%d %d\n",a,b);
-  }   
-  return 0;     
+{ 
+  printf("Hello World");
+  return 0;
 }
